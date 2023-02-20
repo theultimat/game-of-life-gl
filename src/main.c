09dc19a259db46af83d2580ca263b4bcb2f52118
theultimat <theultimat@gmail.com>
2023-02-20 20:08:00 +0000

    Main: Add an initial main loop.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
+#include "window.h"
+
+
+#define GOL_TICKS_PER_SECOND (1.0)
+#define GOL_SECONDS_PER_TICK (1.0 / GOL_TICKS_PER_SECOND)
+
 
 int main(void)
 {
-    printf("hello world\n");
+    GolWindow window;
+    gol_create_window(&window, 1280, 720);
+
+    double previous_time = glfwGetTime();
+    double latency_time = 0.0;
+
+    while (!glfwWindowShouldClose(window.window))
+    {
+        double current_time = glfwGetTime();
+        double elapsed_time = current_time - previous_time;
+
+        previous_time = current_time;
+        latency_time += elapsed_time;
+
+        glfwPollEvents();
+
+        while (latency_time >= GOL_SECONDS_PER_TICK)
+        {
+            // TODO Update state.
+            latency_time -= GOL_SECONDS_PER_TICK;
+        }
+
+        // TODO Draw state.
+        glfwSwapBuffers(window.window);
+    }
+
+    gol_destroy_window(&window);
+
     return 0;
 }
