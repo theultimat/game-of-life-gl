bf9d561993f18e797985b4bd0589b41fd9729b7a
theultimat <theultimat@gmail.com>
2023-02-20 20:50:40 +0000

    Main: Draw the canvas.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+#include "assert.h"
 #include "canvas.h"
 #include "io.h"
 #include "program.h"
@@ -9,6 +13,26 @@
 
 #define GOL_TICKS_PER_SECOND (1.0)
 #define GOL_SECONDS_PER_TICK (1.0 / GOL_TICKS_PER_SECOND)
+
+
+static void draw(const GolWindow *window, const GolCanvas *canvas, const GolProgram *prog)
+{
+    // Note - we don't need to do a clear as we're always going to be writing
+    // to every pixel.
+
+    glViewport(0, 0, window->width, window->height);
+
+    GOL_CHECK_GL();
+
+    glUseProgram(prog->name);
+
+    GOL_CHECK_GL();
+
+    glBindVertexArray(canvas->vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    GOL_CHECK_GL();
+}
 
 
 int main(void)
@@ -50,7 +74,7 @@ int main(void)
             latency_time -= GOL_SECONDS_PER_TICK;
         }
 
-        // TODO Draw state.
+        draw(&window, &canvas, &program);
         glfwSwapBuffers(window.window);
     }
 
